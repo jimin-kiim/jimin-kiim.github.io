7b2077fa1a446a39731342e3c598715e82f4235a
Jimin Kim <0305jimin@gmail.com>
2023-03-18 12:52:38 +0900

    Chore: deleting useless files

--- js/background.js
+++
@@ -1,6 +0,0 @@
-const images=["0.png","1.png","2.png","3.png","4.png"];
-
-const chosenImage = images[Math.floor(Math.random()*images.length)];
-const bgImage = document.querySelector("body");
-
-bgImage.style=`background-image: url("img/background/${chosenImage}")`;
